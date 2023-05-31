84edd4319777a052aa44597fcd29e1fc647a791a
mikeh-dev <mikeh112@hotmail.com>
2023-05-31 01:07:02 +0100

    Added 50 users and 50 engines to Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+50.times do
+  User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+
+# Assuming you have a User model
+users = User.all
+
+50.times do
+  engine = Engine.new(
+    make: Faker::Vehicle.make,
+    model: Faker::Vehicle.model,
+    engine_number: Faker::Number.number(digits: 5)
+  )
+  engine.user = users.sample
+  engine.save!
+end
