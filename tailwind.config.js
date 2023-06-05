2e0fbcc7fd374a5fb7fd64db1e079021fc248754
mikeh-dev <mikeh112@hotmail.com>
2023-06-05 02:12:08 +0100

    More about us page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     theme: {
       extend: {
         fontFamily: {
-            sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+            sans: ['Roboto', 'Arial', 'sans-serif'],
       },
     },
     plugins: [
