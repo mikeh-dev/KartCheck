5948ce872ba284b88ff78f5d4833edb85c38d504
mikeh-dev <mikeh112@hotmail.com>
2023-07-26 14:32:01 +0100

    Combined Search page and search results view for both Chassis and Engine model

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,10 @@ class SearchController < ApplicationController
   def index
     if params[:q].present?
       @engines = Engine.where("LOWER(engine_number) = ?", params[:q].downcase)
+      @chassis = Chassis.where("LOWER(number) = ?", params[:q].downcase)
     else
       @engines = Engine.none
+      @chassis = Chassis.none
     end
   end
 end
